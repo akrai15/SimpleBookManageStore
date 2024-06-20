1cf6084151dd961673b34dfadb25c504244402db
AyushRai <raiayush572@gmail.com>
2024-06-21 01:10:06 +0530

    Deployment Ready

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,16 @@ import authRoutes from './routes/auth.routes.js';
 import bookRoutes from './routes/book.routes.js';
 dotenv.config();
 import cookieParser from 'cookie-parser';
+import cors from 'cors';      
+import path from 'path';
+
 const PORT= process.env.PORT || 5000;
 
 const app = express();
+const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
+
 
 app.use('/api/auth',authRoutes);
 app.use('/api/books',bookRoutes);
@@ -17,9 +22,11 @@ app.use('/api/books',bookRoutes);
 
 
 
+app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
-
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend/dist","index.html"));
+});
 
 
 
